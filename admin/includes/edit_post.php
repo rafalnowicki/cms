1bedff755688bbb732518e61ddfbdc924f243a80
root <root@rafaal.nazwa.pl>
2018-03-23 01:00:01 +0100

    latest update

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -1,40 +1,67 @@
-<form action="" method="post" enctype="multipart/form-data">
+<?php
+global $connection;
 
+if(isset($_GET['p_id'])) {
+    $the_post_id = $_GET['p_id'];
+}
+$query = "SELECT * FROM posts WHERE post_id = {$the_post_id}";
+$select_posts_by_id = mysqli_query($connection, $query);
+
+while($row = mysqli_fetch_assoc($select_posts_by_id)) {
+    $post_id = $row['post_id'];
+    $post_category = $row['post_category_id'];
+    $post_title = $row['post_title'];
+    $post_author = $row['post_author'];
+    $post_status = $row['post_status'];
+    $post_image = $row['post_image'];
+    $post_tags = $row['post_tags'];
+    $post_comment_count = $row['post_comment_count'];
+    $post_date = $row['post_date'];
+    $post_content = $row['post_content'];
+}
+?>
+
+
+
+
+
+
+<form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="post_title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
-        <label for="post_category">Category</label>
-        <input type="text" class="form-control" name="post_category_id">
-<!--
-        <select name="post_category_id" id="3">
-       </select>
--->
 
+   <select name="" id="">
+   
+    <?php
+        
+    $query = "SELECT * FROM categories";
+    $select_categories = mysqli_query($connection, $query);
+        
+//    confirmQuery($select_categories);
+
+    while($row = mysqli_fetch_assoc($select_categories)) {
+        $cat_id = $row['cat_id'];
+        $cat_title = $row['cat_title'];
+    
+    echo "<option value=''>{$cat_title}</option>";
+    
+    }
+        
+    
+       
+    ?>
+    
+   </select>
     </div>
-
-
-<!--
     <div class="form-group">
-        <label for="users">Users</label>
-        <select name="post_user" id="">
-       </select>
-    </div>
--->
-
-
-
-
-
-     <div class="form-group">
          <label for="title">Post Author</label>
-          <input type="text" class="form-control" name="post_author">
-      </div> 
-
-
+          <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
+    </div> 
 
     <div class="form-group">
         <select name="post_status" id="">
@@ -44,22 +71,18 @@
          </select>
     </div>
 
-
-
     <div class="form-group">
-        <label for="post_image">Post Image</label>
-        <input type="file" name="image">
+        <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
     </div>
 
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
-        <input type="text" class="form-control" name="post_tags">
+        <input value="<?php echo $post_tags; ?>" type="text" class="form-control" name="post_tags">
     </div>
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea class="form-control " name="post_content" id="" cols="30" rows="10">
-         </textarea>
+        <textarea value="" class="form-control " name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?></textarea>
     </div>
 
 
