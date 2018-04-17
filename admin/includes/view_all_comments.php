f3fbcdcafdd75c547abba3f28a741e002ab098a3
root <root@rafalnowicki.com>
2018-04-17 02:50:01 +0200

    Latest update

--- admin/includes/view_all_comments.php
+++ admin/includes/view_all_comments.php
@@ -10,7 +10,6 @@
             <th>Date</th>
             <th>Approve</th>
             <th>Unapprove</th>
-            <th>Edit</th>
             <th>Delete</th>
         </tr>
     </thead>
@@ -52,28 +51,49 @@
         
         echo "<td>$comment_email</td>";
         echo "<td>$comment_status</td>";
-        echo "<td></td>";
+        
+        $query = "SELECT * FROM posts WHERE post_id = $comment_post_id";
+        $select_post_id_query = mysqli_query($connection, $query);
+        while($row = mysqli_fetch_assoc($select_post_id_query)) {
+            $post_id = $row['post_id'];
+            $post_title = $row['post_title'];
+            echo "<td><a href='../post.php?p_id=$post_id'>$post_title</a></td>";
+        }
+        
         echo "<td>$comment_date</td>";
-        echo "<td><a href='comments.php?source=edit_comment&p_id={$comment_id}'>Approve</a></td>";
-        echo "<td><a href='comments.php?source=edit_comment&p_id={$comment_id}'>Unapprove</a></td>";
-        echo "<td></td>";
+        echo "<td><a href='comments.php?approve={$comment_id}'>Approve</a></td>";
+        echo "<td><a href='comments.php?unapprove={$comment_id}'>Unapprove</a></td>";
         echo "<td><a href='comments.php?delete={$comment_id}'>Delete</a></td>";
         echo "</tr>";
-
-
     }
 ?>
     </tbody>
 </table>
 
 <?php
-if(isset($_GET['delete'])) {
-    $the_post_id = $_GET['delete'];
-    $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
+
+if(isset($_GET['unapprove'])) {
+    $the_comment_id = $_GET['unapprove'];
+    $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = $the_comment_id";
     $delete_query = mysqli_query($connection, $query);
+    header("Location: comments.php");
 }
 
 
+if(isset($_GET['approve'])) {
+    $the_comment_id = $_GET['approve'];
+    $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = $the_comment_id";
+    $delete_query = mysqli_query($connection, $query);
+    header("Location: comments.php");
+}
+
+
+if(isset($_GET['delete'])) {
+    $the_comment_id = $_GET['delete'];
+    $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id}";
+    $delete_query = mysqli_query($connection, $query);
+    header("Location: comments.php");
+}
 
 
 ?>
--- post.php
+++ post.php
@@ -15,14 +15,9 @@
         <div class="col-md-8">
 
 <?php
-
 if(isset($_GET['p_id'])) {
-    
     $post_id = $_GET['p_id'];
-    
 }
-
-
 $query = "SELECT * FROM posts WHERE post_id = $post_id";
 $select_all_posts_query = mysqli_query($connection, $query);
 
@@ -32,44 +27,43 @@ while($row = mysqli_fetch_array($select_all_posts_query)) {
     $post_date = $row['post_date'];
     $post_image = $row['post_image'];
     $post_content = $row['post_content'];
-
 ?>
 
-<h1 class="page-header">
-    Page Heading
-    <small>Secondary Text</small>
-</h1>
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
 
-<!-- First Blog Post -->
-<h2>
-    <a href="#">
-        <?php echo $post_title; ?>
-    </a>
-</h2>
-<p class="lead">
-    by
-    <a href="index.php">
-        <?php echo $post_author; ?>
-    </a>
-</p>
-<p><span class="glyphicon glyphicon-time"></span>
-    <?php echo $post_date; ?>
-</p>
-<hr>
-<img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
-<hr>
-<p>
-    <?php echo $post_content; ?>
-</p>
-<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#">
+                        <?php echo $post_title; ?>
+                    </a>
+                </h2>
+                <p class="lead">
+                    by
+                    <a href="index.php">
+                        <?php echo $post_author; ?>
+                    </a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span>
+                    <?php echo $post_date; ?>
+                </p>
+                <hr>
+                <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
+                <hr>
+                <p>
+                    <?php echo $post_content; ?>
+                </p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
-<hr>
+                <hr>
 
-<?php } ?>
+                <?php } ?>
 
 
                 <!-- Blog Comments -->
-<?php
+                <?php
 
 if(isset($_POST['create_comment'])) {
     $post_id = $_GET['p_id'];
@@ -88,68 +82,57 @@ if(isset($_POST['create_comment'])) {
 }
 ?>
 
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form action="" method="post" role="form">
-                       <div class="form-group">
-                           <label for="Author">Author</label>
-                            <input type="text" name="comment_author" class="form-control" name="comment_author">
-                        </div>
-                        <div class="form-group">
-                           <label for="Email">Email</label>
-                            <input type="email" name="comment_email" class="form-control" name="comment_email">
-                        </div>
-                        <div class="form-group">
-                           <label for="Comment">Your Comment</label>
-                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-
-                <hr>
-
-                <!-- Posted Comments -->
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    <!-- Comments Form -->
+                    <div class="well">
+                        <h4>Leave a Comment:</h4>
+                        <form action="" method="post" role="form">
+                            <div class="form-group">
+                                <label for="Author">Author</label>
+                                <input type="text" name="comment_author" class="form-control" name="comment_author">
                             </div>
-                        </div>
-                        <!-- End Nested Comment -->
+                            <div class="form-group">
+                                <label for="Email">Email</label>
+                                <input type="email" name="comment_email" class="form-control" name="comment_email">
+                            </div>
+                            <div class="form-group">
+                                <label for="Comment">Your Comment</label>
+                                <textarea name="comment_content" class="form-control" rows="3"></textarea>
+                            </div>
+                            <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
+                        </form>
                     </div>
-                </div>
+
+                    <hr>
+
+                    <!-- Posted Comments -->
+
+<?php
+$query = "SELECT * FROM comments WHERE comment_post_id = {$post_id} ";
+$query .= "AND comment_status = 'approved' ";
+$query .= "ORDER BY comment_id DESC";
+$select_comment_query = mysqli_query($connection, $query);
+if(!$select_comment_query) {
+    die('Query Failed!' . mysqli_error($connection));
+}
+while ($row = mysqli_fetch_array($select_comment_query)) {
+    $comment_date = $row['comment_date'];
+    $comment_conntent = $row['comment_content'];
+    $comment_author = $row['comment_author'];
+?>
+                    <!-- Comment -->
+<div class="media">
+    <a class="pull-left" href="#">
+    <img class="media-object" src="http://placehold.it/64x64" alt="">
+</a>
+    <div class="media-body">
+        <h4 class="media-heading"><?php echo $comment_author; ?>
+            <small><?php echo $comment_date; ?></small>
+        </h4>
+     <?php echo $comment_content; ?>
+    </div>
+</div>
+
+<?php } ?>
 
 
 
