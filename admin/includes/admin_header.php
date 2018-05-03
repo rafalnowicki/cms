e425376c57c7a7cadbba2df7138bcfdccb3aa5c5
root <root@rafalnowicki.com>
2018-05-03 04:00:01 +0200

    Latest update

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,6 +1,15 @@
 <?php ob_start(); ?>
+<?php session_start(); ?>
 <?php include "../includes/db.php" ?>
 <?php include "functions.php" ?>
+
+<?php
+if(!isset($_SESSION['user_role'])) {
+    header("Location: ../index.php");
+} else {
+    
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
